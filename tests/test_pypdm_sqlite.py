0b5070c771fbfbaf03e01310e440991405bf2609
EXP <289065406@qq.com>
2020-08-31 20:48:54 +0800

    优化单元测试

--- tests/test_pypdm_sqlite.py
+++ tests/test_pypdm_sqlite.py
@@ -2,6 +2,7 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
+import shutil
 import unittest
 from src.pypdm.assist.cfg import *
 from src.pypdm import builder
@@ -20,6 +21,7 @@ class TestPypdmSqlite(unittest.TestCase):
     @classmethod
     def tearDownClass(cls) :
         DB_CONN.exec_script('data/rollback_db.sql')
+        shutil.rmtree('src')
 
     def setUp(self) :
         pass
@@ -28,16 +30,20 @@ class TestPypdmSqlite(unittest.TestCase):
         pass
 
     def test_build_pdm(self) :
-        rst = builder.build(
+        paths = builder.build(
             dbtype = SQLITE,
             dbname = DB_PATH,
             pdm_pkg = 'src.pdm.sqlite',
-            table_whitelist = [],
+            table_whitelist = [ 't_teachers', 't_students' ],
             table_blacklist = [ 't_employers', 't_employees' ],
             to_log = True
         )
-        print(rst)
-        # self.assertEqual(rst, True)
+        self.assertEqual(len(paths), 4)
+        self.assertTrue('src/pdm/sqlite/bean/t_teachers.py' in paths)
+        self.assertTrue('src/pdm/sqlite/dao/t_teachers.py' in paths)
+        self.assertTrue('src/pdm/sqlite/bean/t_students.py' in paths)
+        self.assertTrue('src/pdm/sqlite/dao/t_students.py' in paths)
+
 
     def test_bbb(self) :
         print('vvv')
