79abe9c849fde324771bcef68e48a25bc53207dc
EXP <289065406@qq.com>
2020-09-01 02:35:13 +0800

    优化测试用例

--- tests/run_docker_mysql.sh
+++ tests/run_docker_mysql.sh
@@ -1,2 +1,2 @@
 docker pull mysql:latest
-docker run -d --rm -v "$PWD/data/mysql:/var/lib/mysql" -p 3306:3306  --name mysql_test_pypdm -e MYSQL_ROOT_PASSWORD=123456 mysql
+docker run -d --rm -v "$PWD/db/mysql/conf/:/etc/mysql/conf.d/" -v "$PWD/db/mysql/data/:/var/lib/mysql/" -p 3306:3306 --name mysql8 -e MYSQL_ROOT_PASSWORD=123456 mysql
