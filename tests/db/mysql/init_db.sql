5f2d9fdcaddbb0b09d968055c7688266960d9c5c
EXP <289065406@qq.com>
2020-09-01 11:57:01 +0800

    添加 mysql 单元测试

---
+++ tests/db/mysql/init_db.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS `t_teachers` (
+  `i_id` int NOT NULL AUTO_INCREMENT,
+  `s_name` varchar(16) DEFAULT NULL,
+  `s_remark` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`i_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `t_students` (
+  `i_id` int NOT NULL AUTO_INCREMENT,
+  `s_name` varchar(16) DEFAULT NULL,
+  `s_remark` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`i_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `t_employers` (
+  `i_id` int NOT NULL AUTO_INCREMENT,
+  `s_name` varchar(16) DEFAULT NULL,
+  `s_remark` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`i_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `t_employees` (
+  `i_id` int NOT NULL AUTO_INCREMENT,
+  `s_name` varchar(16) DEFAULT NULL,
+  `s_remark` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`i_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `t_teachers`(`s_name`, `s_remark`) VALUES ('apple_1', 'aaa_1');
+INSERT INTO `t_teachers`(`s_name`, `s_remark`)  VALUES ('boy_1', 'bbb_1');
+INSERT INTO `t_teachers`(`s_name`, `s_remark`)  VALUES ('cat_1', 'ccc_1');
+
+INSERT INTO `t_students`(`s_name`, `s_remark`)  VALUES ('apple_2', 'aaa_2');
+INSERT INTO `t_students`(`s_name`, `s_remark`)  VALUES ('boy_2', 'bbb_2');
+INSERT INTO `t_students`(`s_name`, `s_remark`)  VALUES ('cat_2', 'ccc_2');
+
+INSERT INTO `t_employers`(`s_name`, `s_remark`)  VALUES ('apple_3', 'aaa_3');
+INSERT INTO `t_employers`(`s_name`, `s_remark`)  VALUES ('boy_3', 'bbb_3');
+INSERT INTO `t_employers`(`s_name`, `s_remark`)  VALUES ('cat_3', 'ccc_3');
+
+INSERT INTO `t_employees`(`s_name`, `s_remark`)  VALUES ('apple_4', 'aaa_4');
+INSERT INTO `t_employees`(`s_name`, `s_remark`)  VALUES ('boy_4', 'bbb_4');
+INSERT INTO `t_employees`(`s_name`, `s_remark`)  VALUES ('cat_4', 'ccc_4');
