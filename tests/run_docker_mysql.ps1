30c06f7d01c95a091fc4350a594d472dcd6a23fa
EXP <289065406@qq.com>
2020-09-01 11:56:16 +0800

    修正 mysql 镜像

--- tests/run_docker_mysql.ps1
+++ tests/run_docker_mysql.ps1
@@ -1,2 +1,2 @@
-docker pull expm02/mysql:8.1
-docker run -d --rm -v "$PWD/db/mysql/conf/:/etc/mysql/conf.d/" -v "$PWD/db/mysql/data/:/var/lib/mysql/" -p 3306:3306 --name mysql_81 -e MYSQL_ROOT_PASSWORD=123456 expm02/mysql:8.1
+docker pull mysql:latest
+docker run -d --rm -v "$PWD/db/mysql/conf/:/etc/mysql/conf.d/" -v "$PWD/db/mysql/data/:/var/lib/mysql/" -p 3306:3306 --name mysql8 -e MYSQL_ROOT_PASSWORD=123456 mysql
